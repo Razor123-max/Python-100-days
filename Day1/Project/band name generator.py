d06f3bfb84d7083613eda95d1a120616a25750aa
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-10 23:39:57 +0530

    some final touchup

--- Day1/Project/band name generator.py
+++ Day1/Project/band name generator.py
@@ -4,5 +4,5 @@ city_name = input("Whats the name of the city you grew up in?\n")
 
 pet_name = input("Whats the name of the pet you have?\n")
 
-print(city_name + " " + pet_name)
+print("Your brand name could be: " + city_name + " " + pet_name)
 
