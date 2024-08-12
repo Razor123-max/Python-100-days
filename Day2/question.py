71cc5b96268757e58b14f446d5bf8ee905a00ec3
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-12 22:22:31 +0530

    question for data types

---
+++ Day2/question.py
@@ -0,0 +1,13 @@
+print("Number of letteres in your name: " + str(len(input("Enter your name \n"))))
+
+#TODO: Or you can simplfy as
+
+
+name_of_user = input("Enter your name\n")
+length = len(name_of_user)
+
+print("Number of letters in your name: ", length)
+
+#or
+
+print("Number of letters in your name " + str(length))
