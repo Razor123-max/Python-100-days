a67bef8c0065bacd0f5ad5591447fba39d4790d6
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-12 22:51:46 +0530

    maths operation with some extra

---
+++ Day2/mathsoperations.py
@@ -0,0 +1,67 @@
+print("My age:" + str(20))
+
+ #addition
+print(123 + 456)
+
+ #substraction
+print(456 - 123)
+
+ #multiplication
+print(3 * 2)
+
+ # Division with floating point
+print(6 / 2)
+
+ # Division with int
+print(6 // 2)
+
+ #exponential
+print(2 ** 3)
+
+ #Modulo
+print(10 % 3)
+
+ # Floor division
+print(10 // 3)
+
+ # Concatenation
+print("Hello" + " " + "World")
+
+ # Type conversion
+print(str(20) + " years old")
+
+ # Type conversion with float
+print(int(20.5))
+
+ # Length of a string
+print(len("Hello World"))
+
+ # Indexing a string
+print("Hello World"[0])
+
+ # Slicing a string
+print("Hello World"[0:5])
+
+ # Slicing a string with step
+print("Hello World"[0:5:2])
+
+ # String concatenation with multiplication
+print("Hello" * 3)
+
+ # Checking if a string is empty
+print("" == "")
+
+ # Checking if a string contains a substring
+print("Hello" in "Hello World")
+
+ # Checking if a string starts with a substring
+print("Hello" in "Hello World"[:5])
+
+ # Checking if a string ends with a substring
+print("World" in "Hello World")
+
+ # Checking if a string contains a specific character
+print("l" in "Hello World")
+
+ # Checking if a string is a palindrome
+print("radar" == "radar"[::-1])
