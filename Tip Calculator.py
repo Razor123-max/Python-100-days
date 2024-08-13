5c003dca23e1653bdb3a902f62f94ad0a76e6dcb
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-14 00:14:19 +0530

    formula correction

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -1,11 +1,12 @@
-print("Welcome to tip calculator!\n")
+print("\nWelcome to tip calculator!\n")
 
-total_bill = input("What was the total bill?\n")
+total_bill = int(input("What was the total bill?\n"))
 
-tip = input("How much tip would you like to give?\n 10, 12 or 15?\n")
-tip_amount = (total_bill * tip + total_bill)
+tip = int(input("How much tip would you like to give?\n 10, 12 or 15?\n"))
 
-split = input("How many people to split the bill?\n")
+tip_amount = (total_bill * (tip / 100) + total_bill)
+
+split = int(input("How many people to split the bill?\n"))
 split_amount = round(tip_amount / split, 2)
 
 print(f"Each person should pay:{split_amount}\n")
