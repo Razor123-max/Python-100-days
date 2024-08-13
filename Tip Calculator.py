3a843ebf834fff2931ccb3cdf5c35f9027a412d3
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-14 00:15:38 +0530

    adding dollars

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -1,6 +1,6 @@
 print("\nWelcome to tip calculator!\n")
 
-total_bill = int(input("What was the total bill?\n"))
+total_bill = int(input("What was the total bill?\n$"))
 
 tip = int(input("How much tip would you like to give?\n 10, 12 or 15?\n"))
 
@@ -9,4 +9,4 @@ tip_amount = (total_bill * (tip / 100) + total_bill)
 split = int(input("How many people to split the bill?\n"))
 split_amount = round(tip_amount / split, 2)
 
-print(f"Each person should pay:{split_amount}\n")
+print(f"Each person should pay:${split_amount}\n")
