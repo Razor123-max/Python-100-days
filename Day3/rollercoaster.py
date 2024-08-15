768d730899dd44e6646c99a2611e783c9313f4be
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-15 23:43:18 +0530

    elif correction

--- Day3/rollercoaster.py
+++ Day3/rollercoaster.py
@@ -6,9 +6,9 @@ if height >= 120:
     print("\nCongratulations! You are eligible to ride the rollercoaster.\n")
 
     age = int(input("How old are you? "))
-    if age > 18:
-        print("\nYou have to pay 12$\n")
-    elif 18<=age>=12:
+    if age <=12:
+        print("\nYou have to pay 5$\n")
+    elif age >= 18:
         print("\nYou have to pay 7$\n")
 
     else:("\nYou have to pay 5$\n")
