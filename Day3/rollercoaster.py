a10692410a35e8337720745325bdc16d61c7e1b7
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-15 23:18:58 +0530

    done if else

--- Day3/rollercoaster.py
+++ Day3/rollercoaster.py
@@ -2,3 +2,8 @@ print("\nWelcometo the rollercoaster!!!\n")
 
 height = int(input("What is your height in cm? "))
 
+if height > 120:
+    print("\nCongratulations! You are eligible to ride the rollercoaster.\n")
+    
+else:
+    print("\nSorry, you need to grow a little bit taller before you can ride the rollercoaster")
