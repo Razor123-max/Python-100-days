b2bb2093ba3e4767953a8305864c07ab7095c52b
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-15 23:39:55 +0530

    price for different ages

--- Day3/rollercoaster.py
+++ Day3/rollercoaster.py
@@ -6,9 +6,12 @@ if height >= 120:
     print("\nCongratulations! You are eligible to ride the rollercoaster.\n")
 
     age = int(input("How old are you? "))
-    if age >= 18:
-        print("\nYou have to pay 18$\n")
-    else:("\nYou have to pay 7$\n")
+    if age > 18:
+        print("\nYou have to pay 12$\n")
+    if 18<=age>=12:
+        print("\nYou have to pay 7$\n")
+
+    else:("\nYou have to pay 5$\n")
 
 else:
     print("\nSorry, you need to grow a little bit taller before you can ride the rollercoaster")
