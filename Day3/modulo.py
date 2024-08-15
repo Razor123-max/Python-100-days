b072b008767562dd7ed1789396ffc97d24266d59
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-15 23:32:18 +0530

    \n

--- Day3/modulo.py
+++ Day3/modulo.py
@@ -1,6 +1,6 @@
 #check if the entered number is even or odd
 
-number = int(input("Enter the number you want to check if it is even or odd"))
+number = int(input("Enter the number you want to check if it is even or odd\n"))
 
 if number % 2 == 0:
     print(f"The number {number} is even")
