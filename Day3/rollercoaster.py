aa914330900988c6d3c6ae6f2f4a9a84ffe4fdef
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-18 23:48:42 +0530

    age

--- Day3/rollercoaster.py
+++ Day3/rollercoaster.py
@@ -1,14 +1,13 @@
 print("\nWelcometo the rollercoaster!!!\n")
-print
 
 height = int(input("What is your height in cm? "))
 
-if height >= 130:
+if height >= 120:
     print("\nCongratulations! You are eligible to ride the rollercoaster.\n")
 
     age = int(input("How old are you? "))
     if age <=12:
-        print("\nYou have to pay 5$\n")
+        print("\nChil tickets are 5$\n")
     elif age >= 17:
         print("\nYou have to pay 7$\n")
 
