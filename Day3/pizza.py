cf39131877ebab354f29310e69f04efdcdb24060
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-19 23:27:29 +0530

    done

--- Day3/pizza.py
+++ Day3/pizza.py
@@ -1,7 +1,29 @@
 print("\nWelcome to Python Pizza Deliveries!\n")
 
+bill = 0
+
 size = input("What size of pizz do you want? S, M or L:\n")
 
-pepperoni = input("Do you want pepperoni on your pizza? Y or N:\n")
+if size == "S":
+    bill += 15
 
-extra_cheese = input("Do you want extra cheese? Y or N:\n")
+elif size == "M":
+    bill += 20
+
+elif size == "L":
+    bill += 25
+
+pepperoni = input("\nDo you want pepperoni on your pizza? Y or N:\n")
+
+if pepperoni == "Y" and size == "S":
+    bill += 2
+
+elif pepperoni == "Y" and (size == "M" or size == "L"):
+    bill += 3
+
+extra_cheese = input("\nDo you want extra cheese? Y or N:\n")
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"\n Thank you for your order! Your total bill is ${bill}")
