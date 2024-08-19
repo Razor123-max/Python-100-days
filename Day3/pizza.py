03a0a134122259c0d7208f0cf124e4aaecc2a6d1
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-19 23:21:27 +0530

    inputs

---
+++ Day3/pizza.py
@@ -0,0 +1,7 @@
+print("\nWelcome to Python Pizza Deliveries!\n")
+
+size = input("What size of pizz do you want? S, M or L:\n")
+
+pepperoni = input("Do you want pepperoni on your pizza? Y or N:\n")
+
+extra_cheese = input("Do you want extra cheese? Y or N:\n")
