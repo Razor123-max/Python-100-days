3658239d45c2bc22ce477ef92389fbce600a67c1
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-20 23:39:58 +0530

    Create treasureisland.py

---
+++ Day3/treasureisland.py
@@ -0,0 +1,46 @@
+print(''' 
+      
+
+  ,d                                                                       
+  88                                                                       
+MM88MMM 8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,  ,adPPYba, 
+  88    88P'   "Y8 a8P_____88 ""     `Y8 I8[    "" 88       88 88P'   "Y8 a8P_____88
+  88    88         8PP""""""" ,adPPPPP88  `"Y8ba,  88       88 88         8PP""""""" 
+  88,   88         "8b,   ,aa 88,    ,88 aa    ]8I "8a,   ,a88 88         "8b,   ,aa 
+  "Y888 88          `"Ybbd8"' `"8bbdP"Y8 `"YbbdP"'  `"YbbdP'Y8 88          `"Ybbd8"' 
+                                                                           
+                                                                           
+                                                               
+ 88           88                                 88  
+ ""           88                                 88  
+              88                                 88  
+ 88 ,adPPYba, 88 ,adPPYYba, 8b,dPPYba,   ,adPPYb,88  
+ 88 I8[    "" 88 ""     `Y8 88P'   `"8a a8"    `Y88  
+ 88  `"Y8ba,  88 ,adPPPPP88 88       88 8b       88  
+ 88 aa    ]8I 88 88,    ,88 88       88 "8a,   ,d88  
+ 88 `"YbbdP"' 88 `"8bbdP"Y8 88       88  `"8bbdP"Y8 
+
+                            _.--.
+                        _.-'_:-'||
+                    _.-'_.-::::'||
+               _.-:'_.-::::::'  ||
+             .'`-.-:::::::'     ||
+            /.'`;|:::::::'      ||_
+           ||   ||::::::'     _.;._'-._
+           ||   ||:::::'  _.-!oo @.!-._'-.
+           \.  ||:::::.-!()oo @!()@.-'_.|
+            '.'-;|:.-'.&$@.& ()$%-'o.'\U||
+              `>'-.!@%()@'@_%-'_.-o _.|'||
+               ||-._'-.@.-'_.-' _.-o  |'||
+               ||=[ '-._.-\U/.-'    o |'||
+               || '-.]=|| |'|      o  |'||
+               ||      || |'|        _| ';
+               ||      || |'|    _.-'_.-'
+               |'-._   || |'|_.-'_.-'
+            jgs '-._'-.|| |' `_.-'
+                    '-.||_/.-'
+''')
+
+print("\nWelcome to Treasure Island!!\nYour mission is to find the treasure.")
+
+input("\nYou are at a cross road. Where do you want to go?\ntype 'left' or 'right'.")
