ad13733f9636c6ab2e05eabf1955329e0d72db30
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-22 23:42:24 +0530

    heads and tails

--- Day4/random.py
+++ Day4/random.py
@@ -1,9 +1,13 @@
 import random
 
-random_number_0_to_1 = random.variant() * 10
+#random_number_0_to_1 = random.random * 10
 
-print(f"Random number between 0 and 1: {random_number_0_to_1}")
+#print(f"Random number between 0 and 1: {random_number_0_to_1}")
 
-random_float = random.uniform()
+#random_float = random.uniform()
 
-print(f"Random floating point number between 1 and 10: {random_float}")
+#print(f"Random floating point number between 1 and 10: {random_float}")
+
+random_heads_or_tails = random.randint(0,1)
+print("Head")
+print("Tails")
