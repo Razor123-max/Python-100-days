72f4dbc8fd7e5f131e828c85adebfcde40dbc460
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-22 23:25:37 +0530

    floating point

--- Day4/random.py
+++ Day4/random.py
@@ -1,5 +1,9 @@
 import random
 
-random_number_0_to_1 = random.random()
+random_number_0_to_1 = random.random() * 10
 
 print(f"Random number between 0 and 1: {random_number_0_to_1}")
+
+random_float = random.uniform(1,10)
+
+print(f"Random floating point number between 1 and 10: {random_float}")
