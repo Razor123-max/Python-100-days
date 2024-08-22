b997484013286a3ce61ea5d2e82b0f45e558b3dd
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-22 23:30:55 +0530

    still random error

--- Day4/random.py
+++ Day4/random.py
@@ -4,6 +4,6 @@ random_number_0_to_1 = random.variant() * 10
 
 print(f"Random number between 0 and 1: {random_number_0_to_1}")
 
-random_float = random.uniform(a:1,b:10)
+random_float = random.uniform()
 
 print(f"Random floating point number between 1 and 10: {random_float}")
